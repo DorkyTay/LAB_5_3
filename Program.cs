53851166155d816397ab4cc54401fcefb368a2eb
DorkyTay <67008340+DorkyTay@users.noreply.github.com>
2020-07-30 22:45:32 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LAB_5_3
+{
+    class Program
+    {
+        struct Actor
+        {
+            public int actorID;
+            public string firstName;
+            public string lastName;
+            public DateTime lastupdate;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Actor goodActor = new Actor();
+            goodActor.actorID = 1;
+            goodActor.firstName = "Brad";
+            goodActor.lastName = "Pitt";
+            goodActor.lastupdate = DateTime.Now;
+
+            Actor badActor = new Actor
+            {
+                actorID = 2,
+                firstName = "John",
+                lastName = "Smith",
+                lastupdate = DateTime.Now
+            };
+
+            string messageGoodActor;
+            messageGoodActor = $"{goodActor.firstName} {goodActor.lastName} is a very good actor.";
+
+            string messageBadActor;
+            messageBadActor = $"{badActor.firstName} {badActor.lastName} is a very bad actor.";
+
+            Console.WriteLine($"{messageGoodActor} {messageBadActor}");
+
+        }
+    }
+}
